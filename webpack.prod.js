1dcd49218ff3ec0aadbd3b619f8b18e26d7d9cc6
zinianshiluren <a744506701@163.com>
2019-07-25 00:36:36 +0800

    feat: 增加css,js压缩

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
@@ -10,13 +11,38 @@ module.exports = {
         // 还可以多个入口，例如再加一个：
         // search: './src/search.js'
     },
+    performance: {
+        // false | "error" | "warning" // 不显示性能提示 | 以错误形式提示 | 以警告...
+        hints: "warning",
+        // 开发环境设置较大防止警告
+        // 根据入口起点的最大体积，控制webpack何时生成性能提示,整数类型,以字节为单位
+        maxEntrypointSize: 5000000,
+        // 最大单个资源体积，默认250000 (bytes)
+        maxAssetSize: 3000000
+    },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'Output Management'
+            // template: path.join(_dirname,'src/....')
+            // filename: "index.html",
+            // chunks: ['search'],
+            // inject: true,
+            title: 'Output Management',
+            minify: {
+                html5: true,
+                collapseWhitespace: true,
+                preserveLineBreaks: false,
+                minifyCSS: true,
+                minifyJS: true,
+                removeComments: false
+            }
         }),
         new MiniCssExtractPlugin({
             filename: '[name]_[contenthash:8].css'
+        }),
+        new OptimizeCSSAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano')
         })
     ],
     output: {
